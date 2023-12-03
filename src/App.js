5cff56677b8acdfdab380b05e5089a9a96903d16
ashish-pro <ashishbit017@outlook.com>
2023-12-03 23:06:24 +0530

    mui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {Button} from "@mui/material";
+import { Button } from "@mui/material";
 import SettingsIcon from '@mui/icons-material/Settings';
 function App() {
   return (
     <div>
       <Button variant="text">Text</Button>
       <Button variant="contained" color="secondary" startIcon={<SettingsIcon />} endIcon={<SettingsIcon />}>Contained</Button>
-      <Button variant="outlined" disabled>Outlined</Button>
+      <Button variant="outlined" tartIcon={<SettingsIcon />}>Outlined</Button>
     </div>
   );
 }
